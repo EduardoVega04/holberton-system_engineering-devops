42e245f46fab7875fe20bdabae5fba5ab75e5a65
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-30 20:41:15 -0500

    Fix bug in Apache web server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Fix a file that tries to read a .phpp file instead a .php one
 exec { 'fix_file':
-  command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  command => "/bin/grep -l 'phpp' /var/www/html/*.php | /usr/bin/xargs -r /bin/sed -i 's/phpp/php/g'",
 }
+
